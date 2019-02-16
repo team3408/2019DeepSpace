7d9b2ab0b12c834bf25c31d3fd819a648ea83d4e
KyleDerbabian <kylederbabian@lhsla.org>
2019-02-15 16:22:52 -0800

    Update Robot.java

--- Robot.java
+++ Robot.java
@@ -1,3 +1,4 @@
+// 2/15/19
 package frc.robot;
 
 import edu.wpi.first.wpilibj.*;
@@ -10,31 +11,34 @@ public class Robot extends TimedRobot {
   public static SerialPort mySerialPort;
   Joystick driveStick = new Joystick(0);
   Joystick mechStick = new Joystick(1);
-  TalonSRX FL = new TalonSRX(0);
-  TalonSRX BL = new TalonSRX(1);
-  TalonSRX FR = new TalonSRX(2);
-  TalonSRX BR = new TalonSRX(3);
-  TalonSRX leftBall = new TalonSRX(4);
-  TalonSRX rightBall = new TalonSRX(5);
-  TalonSRX lift = new TalonSRX(6);
+  TalonSRX FL = new TalonSRX(3);
+  TalonSRX BL = new TalonSRX(2);
+  TalonSRX FR = new TalonSRX(0);
+  TalonSRX BR = new TalonSRX(1);
+  TalonSRX ballMech = new TalonSRX(4);
+  TalonSRX lift1 = new TalonSRX(5);
+  TalonSRX lift2 = new TalonSRX(6);
   Servo leftServo = new Servo(0);
   Servo rightServo = new Servo(1);
   String command;
+  boolean connected = false;
 
   public static SerialPort serialPort;
  // SmartDashboard dashboard;
 
   @Override
   public void robotInit() {
-    try {
-      mySerialPort = new SerialPort(9600, SerialPort.Port.kMXP);
+   /* try {
+      mySerialPort = new SerialPort(115200, SerialPort.Port.kMXP);
       mySerialPort.setTimeout(0.8);
+      connected = true; //?
     }  
 
     catch (Exception e) {
       DriverStation.getInstance();
       DriverStation.reportWarning("Cannot connect to arduino :(", false);
     }
+    */
   }
 
   @Override
@@ -49,12 +53,16 @@ public class Robot extends TimedRobot {
   public void teleopInit() {
     leftServo.setAngle(180);
     rightServo.setAngle(43);
-  }
+  } 
 
   @Override
   public void teleopPeriodic() { 
+    double mechValue = driveStick.getRawAxis(2);
+		double mechValue1 = driveStick.getRawAxis(3);
+    
     //Vision
-    while(driveStick.getRawButton(4))
+    /*
+    while(driveStick.getRawButton(4) && connected)
     {
       command = serialPort.readString(1);
 
@@ -84,13 +92,15 @@ public class Robot extends TimedRobot {
       }
 
     }
-    
+    */
 
     //Tank Drive
-    FR.set(ControlMode.PercentOutput, driveStick.getRawAxis(1));
-    BR.set(ControlMode.PercentOutput, driveStick.getRawAxis(1));
-    FL.set(ControlMode.PercentOutput, driveStick.getRawAxis(5));
-    BL.set(ControlMode.PercentOutput, driveStick.getRawAxis(5));
+    FL.setInverted(true);
+    BL.setInverted(true);
+    FR.set(ControlMode.PercentOutput, driveStick.getRawAxis(5));
+    BR.set(ControlMode.PercentOutput, driveStick.getRawAxis(5));
+    FL.set(ControlMode.PercentOutput, driveStick.getRawAxis(1));
+    BL.set(ControlMode.PercentOutput, driveStick.getRawAxis(1));
 
     //Disc Mech
     while (driveStick.getRawButton(5)) {
@@ -103,16 +113,19 @@ public class Robot extends TimedRobot {
     }
 
     //Ball Mech
-    while (mechStick.getRawAxis(2) > 0) {
-      leftBall.set(ControlMode.PercentOutput, mechStick.getRawAxis(2));
-      rightBall.set(ControlMode.PercentOutput, mechStick.getRawAxis(2));
-    }
-    
-    while (mechStick.getRawAxis(3) > 0) {
-      leftBall.set(ControlMode.PercentOutput, -mechStick.getRawAxis(3));
-      rightBall.set(ControlMode.PercentOutput, -mechStick.getRawAxis(3));
-    }
-
+    if (mechValue1 > 0) {
+      ballMech.set(ControlMode.PercentOutput, (0.5)*(mechValue1*mechValue1));
+      
+      }
+  
+      else {
+        ballMech.set(ControlMode.PercentOutput, 0);
+      }
+      if (mechValue > 0) {
+      ballMech.set(ControlMode.PercentOutput, (0.5)*(-mechValue*mechValue));
+      mechValue1 = driveStick.getRawAxis(3);
+      }
+  
     //Lift Mech
     while (mechStick.getRawButton(1)) {
       
